91a76801f1beef2cc2d8ac357d4a9a4b14cb0da2
Drew Johnson <drew.johnson.dev@gmail.com>
2016-04-24 16:43:41 +0100

    KT: Add initial Knight spec

---
+++ 9-knights_travails/spec/knight_spec.rb
@@ -0,0 +1,16 @@
+require './lib/knight'
+
+describe Knight do
+  it 'has a colour'
+
+  describe '#valid?' do
+    context 'when given a valid move' do
+      it 'returns true'
+    end
+
+    context 'when given an invalid move' do
+      it 'returns true'
+    end
+
+  end
+end
