f31dd4122c69e2ff2996959a71192a30c64681bf
Drew Johnson <drew.johnson.dev@gmail.com>
2016-04-25 10:03:30 +0100

    Connect 4: Add validity check for spot coordinates

--- 10-connect4/lib/grid.rb
+++ 10-connect4/lib/grid.rb
@@ -13,13 +13,18 @@ class Grid
     @columns[column][spot] = counter
   end
 
+  def on_grid?(spot)
+    return false unless spot.first.between?(0, 6) || spot.last.between?(0, 5)
+    true
+  end
+
   def has_a_line?
     return true if linear_check?(columns)
     return true if linear_check?(rows)
     false
   end
 
-  def linear_check?(grid)
+  def linear_check?(grid) 
     value = ""
     grid.each do |line|
       count = 0
@@ -35,6 +40,24 @@ class Grid
       end
     end
     return false
+  end
+
+  def linear_spot_check?(spot)
+    stack = []
+
+    spotnode = {position: spot, neighbours: [], depth: 1}
+  end
+
+  def neighbours
+    [[-1, 1], [0, 1], [1, 1],
+     [-1, 0],         [1, 0],
+     [-1,-1], [0,-1], [1,-1]]
+  end
+
+  def spots_adjacent_to(spot)
+    spots = neighbours.map do |neighbour|
+              spot.zip(neighbour).map { |s,n| s + n }
+            end.select { |spot| spot.valid? }
   end
 
   def rows
--- 10-connect4/spec/grid_spec.rb
+++ 10-connect4/spec/grid_spec.rb
@@ -27,27 +27,62 @@ describe Grid do
     end
   end
 
+  describe 'on_grid?' do 
+    it 'returns true for an empty spot' do
+      expect(subject.on_grid?([3,3])).to be true  
+    end
+
+    it 'returns true for a full spot' do
+      subject.place_counter(3, 'X')
+      expect(subject.on_grid?([3,0])).to be true  
+    end
+
+    it 'returns false for a spot off the board' do
+      expect(subject.on_grid?([7,6])).to be false  
+    end
+  end
+
   describe '#has_a_line?' do
     it 'returns true when there are four in a column' do
       4.times { subject.place_counter(3, "X") }
-      expect(subject.has_a_line?).to be true
+
+      expect(subject).to have_a_line
+    end
+
+    it 'does not carry count over between columns' do
+      3.times { subject.place_counter(3, 'O') }
+      3.times { subject.place_counter(3, 'X') }
+      subject.place_counter(4, 'X')
+      3.times { subject.place_counter(4, 'O') }
+
+      expect(subject).not_to have_a_line
     end
 
     it 'returns false when there are not four in a column' do
       3.times { subject.place_counter(3, "X") }
       3.times { subject.place_counter(3, "O") }
-      expect(subject.has_a_line?).to be false
+
+      expect(subject).not_to have_a_line
     end
 
     it 'returns true when there are four in a row' do
       (0..3).each {|n| subject.place_counter(n, 'X')}
-      expect(subject.has_a_line?).to be true
+
+      expect(subject).to have_a_line
     end
 
     it 'returns false when there are not four in a row' do
       (0..2).each {|n| subject.place_counter(n, 'X')}
       (3..5).each {|n| subject.place_counter(n, 'O')}
-      expect(subject.has_a_line?).to be false
+
+      expect(subject).not_to have_a_line
+    end
+
+    it 'returns true when there are four in a diagonal' do
+      (1..3).each { |n| n.times { subject.place_counter(n ,'O') } }
+      (0..3).each { |n| subject.place_counter(n, 'X') }
+
+      expect(subject).to have_a_line
     end
 
     # context 'with four counters in a row' do
